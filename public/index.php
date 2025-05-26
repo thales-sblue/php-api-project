c3aa58bfc37298289dba576ca69c6832b046b085
thales-sblue <thales_sblue@hotmail.com>
2025-05-26 18:48:43 -0300

    feat: setting firts routes

--- public/index.php
+++ public/index.php
@@ -1,8 +1,27 @@
 <?php
 require_once __DIR__ . '/../src/Database/Database.php';
+require_once __DIR__ . '/../src/Controller/UserController.php';
+require_once __DIR__ . '/../src/Controller/TaskController.php';
 
 header('Content-Type: application/json');
 
-echo json_encode([
-    'message' => 'API esta rodando!'
-]);
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$uri = explode('/', $uri);
+$route = $uri[1] ?? '';
+
+switch ($route) {
+    case 'users':
+        $controller = new UserController();
+        $controller->handleRequest($_SERVER['REQUEST_METHOD'], $uri);
+        break;
+
+    case 'tasks':
+        $controller = new TaskController();
+        $controller->handleRequest($_SERVER['REQUEST_METHOD'], $uri);
+        break;
+
+    default:
+        http_response_code(404);
+        echo json_encode(['error' => 'Not Found']);
+        break;
+}
